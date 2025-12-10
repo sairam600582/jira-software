4983414823527440bd39f80dffdb2c5fcfb00461
Sairam Bisai <bisaisairam143p@gmail.com>
2025-12-10 12:32:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,51 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://your-domain.atlassian.net/rest/api/3/project"
-
-auth = HTTPBasicAuth("email@example.com", "<api_token>")
+url = "https://sairam0.atlassian.net/rest/api/3/issue"
+API_TOKEN = "" //USER TOKEN
+auth = HTTPBasicAuth("bisaisairam143p@gmail.com", API_TOKEN)
 
 headers = {
-  "Accept": "application/json"
+  "Accept": "application/json",
+  "Content-Type": "application/json"
 }
 
+payload = json.dumps( {
+  "fields": {
+    "project": {
+      "key": "FRON"
+    },
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "this is own my fault .",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },       
+    "issuetype": {
+      "name": "Story"
+    },
+
+    "summary": "Create it own fault",   
+  },
+  "update": {}
+} )
+
 response = requests.request(
-   "GET",
+   "POST",
    url,
+   data=payload,
    headers=headers,
    auth=auth
 )
